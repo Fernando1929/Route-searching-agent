659ea2defff0134bef595b97c632d9f57a5546fa
Julibert <julibert.diaz@upr.edu>
2020-05-14 00:54:27 -0400

    Expanded the number of locations in a second set of csv files

--- main/agent.py
+++ main/agent.py
@@ -2,7 +2,10 @@
 # Source Code: aima-python
 # https://github.com/aimacode/aima-python/blob/master/agents.py
 
-#utility and annealing will be the focus, to be fixed later
+# utility and annealing will be the focus, to be fixed later
+import collections
+from random import random
+
 
 class Thing:
     """This represents any physical object that can appear in an Environment.
@@ -58,73 +61,6 @@ class Agent(Thing):
         return False
 
 
-def TraceAgent(agent):
-    """Wrap the agent's program to print its input and output. This will let
-    you see what the agent is doing in the environment."""
-    old_program = agent.program
-
-    def new_program(percept):
-        action = old_program(percept)
-        print('{} perceives {} and does {}'.format(agent, percept, action))
-        return action
-
-    agent.program = new_program
-    return agent
-
-
-# ______________________________________________________________________________
-
-
-def TableDrivenAgentProgram(table):
-    """
-    [Figure 2.7]
-    This agent selects an action based on the percept sequence.
-    It is practical only for tiny domains.
-    To customize it, provide as table a dictionary of all
-    {percept_sequence:action} pairs.
-    """
-    percepts = []
-
-    def program(percept):
-        percepts.append(percept)
-        action = table.get(tuple(percepts))
-        return action
-
-    return program
-
-
-def RandomAgentProgram(actions):
-    """An agent that chooses an action at random, ignoring all percepts.
-    >>> list = ['Right', 'Left', 'Suck', 'NoOp']
-    >>> program = RandomAgentProgram(list)
-    >>> agent = Agent(program)
-    >>> environment = TrivialVacuumEnvironment()
-    >>> environment.add_thing(agent)
-    >>> environment.run()
-    >>> environment.status == {(1, 0): 'Clean' , (0, 0): 'Clean'}
-    True
-    """
-    return lambda percept: random.choice(actions)
-
-
-# ______________________________________________________________________________
-
-
-def SimpleReflexAgentProgram(rules, interpret_input):
-    """
-    [Figure 2.10]
-    This agent takes action based solely on the percept.
-    """
-
-    def program(percept):
-        state = interpret_input(percept)
-        rule = rule_match(state, rules)
-        action = rule.action
-        return action
-
-    return program
-
-
 def ModelBasedReflexAgentProgram(rules, update_state, model):
     """
     [Figure 2.12]
@@ -146,96 +82,3 @@ def rule_match(state, rules):
     for rule in rules:
         if rule.matches(state):
             return rule
-
-
-# ______________________________________________________________________________
-
-
-loc_A, loc_B = (0, 0), (1, 0)  # The two locations for the Vacuum world
-
-
-def RandomVacuumAgent():
-    """Randomly choose one of the actions from the vacuum environment.
-    >>> agent = RandomVacuumAgent()
-    >>> environment = TrivialVacuumEnvironment()
-    >>> environment.add_thing(agent)
-    >>> environment.run()
-    >>> environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
-    True
-    """
-    return Agent(RandomAgentProgram(['Right', 'Left', 'Suck', 'NoOp']))
-
-
-def TableDrivenVacuumAgent():
-    """Tabular approach towards vacuum world as mentioned in [Figure 2.3]
-    >>> agent = TableDrivenVacuumAgent()
-    >>> environment = TrivialVacuumEnvironment()
-    >>> environment.add_thing(agent)
-    >>> environment.run()
-    >>> environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
-    True
-    """
-    table = {((loc_A, 'Clean'),): 'Right',
-             ((loc_A, 'Dirty'),): 'Suck',
-             ((loc_B, 'Clean'),): 'Left',
-             ((loc_B, 'Dirty'),): 'Suck',
-             ((loc_A, 'Dirty'), (loc_A, 'Clean')): 'Right',
-             ((loc_A, 'Clean'), (loc_B, 'Dirty')): 'Suck',
-             ((loc_B, 'Clean'), (loc_A, 'Dirty')): 'Suck',
-             ((loc_B, 'Dirty'), (loc_B, 'Clean')): 'Left',
-             ((loc_A, 'Dirty'), (loc_A, 'Clean'), (loc_B, 'Dirty')): 'Suck',
-             ((loc_B, 'Dirty'), (loc_B, 'Clean'), (loc_A, 'Dirty')): 'Suck'}
-    return Agent(TableDrivenAgentProgram(table))
-
-
-def ReflexVacuumAgent():
-    """
-    [Figure 2.8]
-    A reflex agent for the two-state vacuum environment.
-    >>> agent = ReflexVacuumAgent()
-    >>> environment = TrivialVacuumEnvironment()
-    >>> environment.add_thing(agent)
-    >>> environment.run()
-    >>> environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
-    True
-    """
-
-    def program(percept):
-        location, status = percept
-        if status == 'Dirty':
-            return 'Suck'
-        elif location == loc_A:
-            return 'Right'
-        elif location == loc_B:
-            return 'Left'
-
-    return Agent(program)
-
-
-def ModelBasedVacuumAgent():
-    """An agent that keeps track of what locations are clean or dirty.
-    >>> agent = ModelBasedVacuumAgent()
-    >>> environment = TrivialVacuumEnvironment()
-    >>> environment.add_thing(agent)
-    >>> environment.run()
-    >>> environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
-    True
-    """
-    model = {loc_A: None, loc_B: None}
-
-    def program(percept):
-        """Same as ReflexVacuumAgent, except if everything is clean, do NoOp."""
-        location, status = percept
-        model[location] = status  # Update the model here
-        if model[loc_A] == model[loc_B] == 'Clean':
-            return 'NoOp'
-        elif status == 'Dirty':
-            return 'Suck'
-        elif location == loc_A:
-            return 'Right'
-        elif location == loc_B:
-            return 'Left'
-
-    return Agent(program)
-
-
